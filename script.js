c1c1e484e03da885afd4299b1fe766d204054682
Guilherme <75100660+guizeraSK@users.noreply.github.com>
2022-03-24 21:35:19 -0300

    atualizações pequenas

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const body = document.querySelector("body"),
     toggle = document.querySelector(".toggle"),
     searchBtn = document.querySelector(".search-box"),
     modeSwitch = document.querySelector(".toggle-switch"),
-    modeText = document.querySelector(".mmode-text");
+    modeText = document.querySelector(".mode-text");
+    
+    toggle.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
+    });
 
     modeSwitch.addEventListener("click", () =>{
         body.classList.toggle("dark");
+        
+        if(body.classList.contains("dark")){
+            modeText.innerText = "Light Mode"
+        }else{
+            modeText.innerText = "Dark Mode"
+        }
     });
